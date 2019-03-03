d6178925384f260fd94c5898908bebb7ca7a73d9
Michael Jean <michaeljean@Michaels-MacBook-Pro.local>
2019-03-03 17:22:08 -0500

    Updated cors

--- latintoolkit-backend/config/initializers/cors.rb
+++ latintoolkit-backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3000', 'https://api.latinlearninghub.com']
+    origins ['localhost:3000', 'https://www.latinlearninghub.com']
 
     resource '*',
       headers: :any,
